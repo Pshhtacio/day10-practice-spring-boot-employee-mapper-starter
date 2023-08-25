53f604cdfd171caf78ccbc9447e61c800c0b769f
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-25 11:36:20 +0800

    feat: create class for mapper

---
+++ src/main/java/com/afs/restapi/service/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.afs.restapi.service.mapper;
+
+import com.afs.restapi.entity.Employee;
+import com.afs.restapi.service.dto.EmployeeRequest;
+import com.afs.restapi.service.dto.EmployeeResponse;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
+
+public class EmployeeMapper {
+
+    private EmployeeMapper(){
+    }
+
+    public static Employee toEntity(EmployeeRequest employeeRequest) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeRequest, employee);
+        return employee;
+    }
+
+    public static EmployeeResponse toResponse(Employee employee) {
+        EmployeeResponse employeeResponse = new EmployeeResponse();
+        BeanUtils.copyProperties(employee, employeeResponse);
+        return  employeeResponse;
+    }
+}
