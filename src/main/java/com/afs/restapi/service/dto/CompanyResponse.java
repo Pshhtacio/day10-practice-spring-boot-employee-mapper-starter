2e199157e1000ff5f8208f61e4d6fb669bc1e007
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-25 13:41:29 +0800

    feat: create a new class CompanyResponse

---
+++ src/main/java/com/afs/restapi/service/dto/CompanyResponse.java
@@ -0,0 +1,34 @@
+package com.afs.restapi.service.dto;
+
+public class CompanyResponse {
+    private Long Id;
+    private String name;
+    private Integer employeesCount;
+
+    public CompanyResponse() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEmployeesCount() {
+        return employeesCount;
+    }
+
+    public void setEmployeesCount(Integer employeesCount) {
+        this.employeesCount = employeesCount;
+    }
+}
